ed42ed90ea7374233e75638fa090fe98bce0cf21
洪金敏 <hongjinmin.hjm@alibaba-inc.com>
2023-02-07 11:48:17 +0800

    简单http file server

--- main.go
+++ main.go
@@ -11,21 +11,28 @@ var (
 	ip           string
 	port         string
 	staticFolder string
+	debugFlag    bool
 )
 
 func main() {
-	ip = *flag.String("bind_ip", "0.0.0.0", "server ip")
-	port = *flag.String("port", "12155", "server port")
-	staticFolder = *flag.String("static", "./static/", "static folder path")
+	flag.StringVar(&ip, "bind_ip", "0.0.0.0", "server ip")
+	flag.StringVar(&port, "port", "12155", "server port")
+	flag.StringVar(&staticFolder, "static", "./static/", "static folder path")
+	flag.BoolVar(&debugFlag, "debug", false, "debug mode")
 	flag.Parse()
 
+	if debugFlag {
+		gin.SetMode(gin.DebugMode)
+	} else {
+		gin.SetMode(gin.ReleaseMode)
+	}
 	r := gin.Default()
-    r.Static("/", staticFolder)
+	r.Static("/", staticFolder)
 	ipAdd := net.JoinHostPort(ip, port)
+	fmt.Printf("start server at %s \n", ipAdd)
 	err := r.Run(ipAdd)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-	fmt.Println("启动成功...")
 }
