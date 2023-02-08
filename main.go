a99893e76a6c9ce48ba040a8e221933bf280005b
洪金敏 <hongjinmin.hjm@alibaba-inc.com>
2023-02-08 16:43:26 +0800

    新增配置读取

--- main.go
+++ main.go
@@ -1,9 +1,10 @@
 package main
 
 import (
-	"flag"
 	"fmt"
 	"github.com/gin-gonic/gin"
+	"github.com/spf13/pflag"
+	"github.com/spf13/viper"
 	"net"
 )
 
@@ -12,14 +13,32 @@ var (
 	port         string
 	staticFolder string
 	debugFlag    bool
+
+	err error
 )
 
 func main() {
-	flag.StringVar(&ip, "bind_ip", "0.0.0.0", "server ip")
-	flag.StringVar(&port, "port", "12155", "server port")
-	flag.StringVar(&staticFolder, "static", "./static/", "static folder path")
-	flag.BoolVar(&debugFlag, "debug", false, "debug mode")
-	flag.Parse()
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath("./config")
+	viper.SetConfigName("config")
+	err = viper.ReadInConfig()
+	if err != nil {
+		fmt.Println("read config error...")
+	}
+
+	pflag.String("bind_ip", "0.0.0.0", "server ip")
+	pflag.String("port", "12155", "server port")
+	pflag.String("static", "./static", "static folder path")
+	pflag.Bool("debug", false, "debug mode")
+	pflag.Parse()
+	err = viper.BindPFlags(pflag.CommandLine)
+	if err != nil {
+		fmt.Println("read params from command line error...")
+	}
+	ip = viper.GetString("bind_ip")
+	port = viper.GetString("port")
+	staticFolder = viper.GetString("static")
+	debugFlag = viper.GetBool("debug")
 
 	if debugFlag {
 		gin.SetMode(gin.DebugMode)
@@ -29,8 +48,9 @@ func main() {
 	r := gin.Default()
 	r.Static("/", staticFolder)
 	ipAdd := net.JoinHostPort(ip, port)
+
 	fmt.Printf("start server at %s \n", ipAdd)
-	err := r.Run(ipAdd)
+	err = r.Run(ipAdd)
 	if err != nil {
 		fmt.Println(err)
 		return
